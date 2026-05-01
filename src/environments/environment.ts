d73f29907f3087f666896b7d7dc7af00fb3cc322
AnjarHidayatulloh <anjarhidayatulloh@gmail.com>
2026-05-02 00:12:00 +0700

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCYuHH54jxGVvVZRU61TXNcfirvYQcmYj8",
+    authDomain: "bundatenang.firebaseapp.com",
+    projectId: "bundatenang",
+    storageBucket: "bundatenang.firebasestorage.app",
+    messagingSenderId: "752663002801",
+    appId: "1:752663002801:web:bb883d27ae7e6cfa16ac90"
+  }
+};
